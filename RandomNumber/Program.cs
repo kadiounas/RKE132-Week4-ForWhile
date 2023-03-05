3cd5b34fd6af4c518c120df8e842bc9b336b25ba
kadiounas <kadi.ounas@tktk.ee>
2023-03-05 21:44:59 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd = new Random();
+
+int myRandomNum;
+int randomSum = 0;
+
+for(int i = 0; i < 5; i++)
+{
+    myRandomNum = rnd.Next(0, 11); // genereeritakse väärtusi kuni 10ni, 
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+Console.WriteLine($"Random sum total: {randomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,10 @@
+﻿//for tsükkel
+//Dry-principle - do not repeat yourself
+
+
+for(int i = 0; i < 5; i++) //i++ sama mis i+1
+{
+    Console.WriteLine("We don't need no education");
+}
+
+Console.WriteLine("Have a nice day!");
